358a660a0a7d78927c6829944aa1c66404dc34db
Tom Vaughan <tvaughan@tocino.cl>
2014-05-18 17:03:26 -0400

    Bump dependencies to Jetty 9.

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject ring/ring-jetty-adapter "1.3.0-beta2"
-  :description "Ring Jetty adapter."
-  :url "https://github.com/ring-clojure/ring"
-  :scm {:dir ".."}
+(defproject tvaughan/ring-jetty9-adapter "0.1.0-SNAPSHOT"
+  :description "Ring Jetty 9 adapter."
+  :url "https://github.com/tvaughan/ring-jetty9-adapter"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[ring/ring-core "1.3.0-beta2"]
                  [ring/ring-servlet "1.3.0-beta2"]
-                 [org.eclipse.jetty/jetty-server "7.6.13.v20130916"]]
+                 [org.eclipse.jetty/jetty-server "9.2.0.RC0"]]
   :profiles
-  {:dev {:dependencies [[clj-http "0.6.4"]]}
+  {:dev {:dependencies [[clj-http "0.9.1"]]}
    :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
    :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
    :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}})
